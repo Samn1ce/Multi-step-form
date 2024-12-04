7f6c476a7a97ad15610076bdf9ffe3c6469cd23a
Ifeoluwa <iamsamnice@gmail.com>
2024-12-04 13:29:57 -0800

    Completed

--- form/src/Views/HomeView.jsx
+++ form/src/Views/HomeView.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 function HomeView() {
   return (
     <main className="border-2 border-black flex gap-2 flex-col justify-center items-center h-screen bg-[url('./assets/bg-sidebar-desktop.svg')">
-      <h1 className="text-3xl font-bold">Welcome To YAPSVILLE</h1>
+      <h1 className="text-3xl font-bold">MULTI STEP FORM</h1>
       <NavLink to="/layout">
         <button className="w-40 h-10 border rounded border-focus">
           Create Account
